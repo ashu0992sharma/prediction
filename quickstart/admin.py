8adf9815cd41258b8065f23b94381b2cba53d350
ashutosh <ashutosh.sharma@kuliza.com>
2017-10-23 14:55:30 +0530

    quickstart app removed

--- quickstart/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from . import models
-# Register your models here.
-
-admin.site.register(models.Hostel)
-admin.site.register(models.Gallery)
-admin.site.register(models.Room)
-admin.site.register(models.Notices)
--- quickstart/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-
-from . import views
-
-urlpatterns = [
-    url(r'^room/', views.TestView.as_view(), name='test-view'),
-    url(r'^hostel/', views.HostelView.as_view(), name='hostel-view'),
-]
